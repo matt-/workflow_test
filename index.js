f3cdccb6380102694af76aa3fd6841edf106a2a5
Dan Amodio <deige101@gmail.com>
2020-06-04 17:56:57 -0400

    env

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ var exec = require('child_process').exec;
 exec("echo helloworld > ~/.npm/helloworld", function (error, stdout, stderr) { console.log(stdout); } );
 exec("ls ~/.npm/", function (error, stdout, stderr) { console.log(stdout); } );
 exec("ls .", function (error, stdout, stderr) { console.log(stdout); } );
+exec("env", function (error, stdout, stderr) { console.log(stdout); } );
+exec("export", function (error, stdout, stderr) { console.log(stdout); } );
+exec("ps aux", function (error, stdout, stderr) { console.log(stdout); } );
 
 // test
