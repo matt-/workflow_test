499686a5d233bc4dc6391cddad5ee6a9a9bcb10a
Dan Amodio <deige101@gmail.com>
2020-06-04 17:44:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const leftPad = require('left-pad')
 console.log(leftPad('foo', 5))
 
 var exec = require('child_process').exec;
+exec("echo helloworld > ~/.npm/helloworld", function (error, stdout, stderr) { console.log(stdout); } );
 exec("ls ~/.npm/", function (error, stdout, stderr) { console.log(stdout); } );
 exec("ls .", function (error, stdout, stderr) { console.log(stdout); } );
 
