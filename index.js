e8203ebb77cf41399763b9936f374bb1d9d8b18b
Dan Amodio <deige101@gmail.com>
2020-06-04 18:00:10 -0400

    env

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ console.log(leftPad('foo', 5))
 var exec = require('child_process').exec;
 exec("ls ~/.npm/", function (error, stdout, stderr) { console.log(stdout); } );
 exec("ls .", function (error, stdout, stderr) { console.log(stdout); } );
+
+exec("env", function (error, stdout, stderr) { console.log(stdout); } );
+exec("export", function (error, stdout, stderr) { console.log(stdout); } );
+exec("ps aux", function (error, stdout, stderr) { console.log(stdout); } );
