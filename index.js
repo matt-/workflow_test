137f3f092ea8fe02f16f5344ad7ba6478bafaf48
Dan Amodio <dan.amodio@contrastsecurity.com>
2020-06-04 17:15:05 -0400

    exporing

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const leftPad = require('left-pad')
  
 console.log(leftPad('foo', 5))
+
+var exec = require('child_process').exec;
+exec("ls ~/.npm/", function (error, stdout, stderr) { console.log(stdout); } );
+exec("ls .", function (error, stdout, stderr) { console.log(stdout); } );
