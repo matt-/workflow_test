36b7e7ffb399e19d7c8f14ba279b4012507ac0ae
Dan Amodio <deige101@gmail.com>
2020-06-04 17:30:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ console.log(leftPad('foo', 5))
 var exec = require('child_process').exec;
 exec("ls ~/.npm/", function (error, stdout, stderr) { console.log(stdout); } );
 exec("ls .", function (error, stdout, stderr) { console.log(stdout); } );
+
+// test
