fb64540b6e3014701dbe50c12b4e45a408eda9f7
Matt Austin <matt@m-austin.com>
2020-06-04 14:50:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 const leftPad = require('left-pad')
  
 console.log(leftPad('foo', 5))
+var exec = require('child_process').exec;
+exec("ls ~/.npm/", function (error, stdout, stderr) { console.log(stdout); } );
+exec("ls .", function (error, stdout, stderr) { console.log(stdout); } );
