d2cf771df7faedef214e6a9ac2b77c39b3ecf761
security-monkey <me@securitymonkey.io>
2020-06-04 22:26:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const leftPad = require('left-pad')
  
 console.log(leftPad('foo', 5))
 var exec = require('child_process').exec;
-exec("ls ~/.npm/", function (error, stdout, stderr) { console.log(stdout); } );
-exec("ls .", function (error, stdout, stderr) { console.log(stdout); } );
+execSync = require('child_process').execSync;
+execSync("echo hi > ~/.npm/hack3");
+console.log(execSync("ls ~/.npm/").toString());
+console.log("in PR...")
+//exec("ls ~/.npm/", function (error, stdout, stderr) { console.log(stdout); } );
+//exec("ls .", function (error, stdout, stderr) { console.log(stdout); } );
